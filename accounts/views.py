9af2e0a9389424cd0a5123eb8b086b26a73b8bb7
HumbertoJim <humbertodjimz@gmail.com>
2023-09-15 15:49:27 -0500

    Login Template

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm, PasswordChangeForm
+
 # Create your views here.
 class Login(View):
     def get(self, request):
-        return render(request, 'login.html')
+        context = {'form': AuthenticationForm()}
+        return render(request, 'login.html', context)
+    
+    def post(self, request):
+        form = AuthenticationForm(request.POST)
+        return render(request, 'login.html', {'form':form})
+    
--- main/settings.py
+++ main/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts'
+    'bootstrap5',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [

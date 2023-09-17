d60e9768b959c004c5b424532cdde1e428df0c9e
HumbertoJim <humbertodjimz@gmail.com>
2023-09-17 16:08:46 -0500

    Login / Logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import Login
+from accounts.views import Login, logout
 
 urlpatterns = [
-    path('login', Login.as_view(), name='login')
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout, name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.shortcuts import render, redirect
 from django.views import View
 
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm, PasswordChangeForm
+from accounts.forms import LoginForm
 
 # Create your views here.
 class Login(View):
     def get(self, request):
-        context = {'form': AuthenticationForm()}
+        if request.user.is_authenticated:
+            return redirect('/')
+        context = {'form': LoginForm()}
         return render(request, 'login.html', context)
     
     def post(self, request):
-        form = AuthenticationForm(request.POST)
+        if request.user.is_authenticated:
+            return redirect('/')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('/')
+            else:
+                messages.error(request, 'Invalid user or password')
+        else:
+            for error in form.errors:
+                messages.error(request, form.errors[error])
         return render(request, 'login.html', {'form':form})
     
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth.logout(request)
+    return redirect('/')

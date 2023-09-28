88b689cf8f1aafb3097beb88ae68f3fdbc156741
HumbertoJim <humbertodjimz@gmail.com>
2023-09-28 17:31:10 -0500

    Charts - Inicio

--- main/exceptions.py
+++ main/exceptions.py
@@ -1,9 +1,9 @@
 class NotAuthenticated(Exception):
-    def __init__(self, mensaje='Not authenticated'):
-        self.mensaje = mensaje
-        super().__init__(self.mensaje)
+    def __init__(self, message='Not authenticated'):
+        self.message = message
+        super().__init__(self.message)
 
 class DuplicatedValue(Exception):
-    def __init__(self, mensaje='Duplicated value'):
-        self.mensaje = mensaje
-        super().__init__(self.mensaje)
+    def __init__(self, message='Duplicated value'):
+        self.message = message
+        super().__init__(self.message)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
+from records.models import Record
+import pandas as pd
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'labels': [], 'values':[]}
+    if request.user.is_authenticated:
+        df = pd.DataFrame(list(Record.objects.filter(user=request.user).values('title', 'datetime', 'is_income', 'value', 'category__name', 'category__color')))
+        df = df.rename(columns={
+            'category__name': 'category',
+            'category__color': 'color'
+        })
+        expenses = df[df['is_income']==False]
+        expenses = expenses.groupby('category').sum('value').reset_index()
+        print(expenses.head())
+        context['labels'] =  expenses['category'].values
+        context['values'] = expenses['value'].values
+    return render(request, 'home.html', context)
